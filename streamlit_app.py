ef388ee79063c91d53d50ac18bfaeab5cf24ef1e
Khalid Rehman <khalidrehman83@hotmail.com>
2023-01-13 18:53:48 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3")
-streamlit.text("KAle")
-streamlit.text("Hard-Boiled")
+streamlit.text(" 🥣 Omega 3")
+streamlit.text(" 🥗 KAle")
+streamlit.text(" 🐔 Hard-Boiled Eggs")
+streamlit.text(" 🥑🍞 Avacado Toast")
